4ad6e4d63a46f2fa2c787119389ba410b7b1ff62
Marc Ferland <mferland1980@gmail.com>
2020-01-02 18:49:31 -0500

    Added branch==master condition to push to docker hub

--- docker-push.sh
+++ docker-push.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker push $1/$2_nginx:latest
-docker push $1/$2_php:latest
+if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+    docker push $1/$2_nginx:latest
+    docker push $1/$2_php:latest
+fi
