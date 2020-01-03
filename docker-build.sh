d9dcd6b4227e992d2be3956021d8740d9ffc5a3f
Marc Ferland <mferland1980@gmail.com>
2020-01-03 08:34:21 -0500

    added proper tag for docker hub

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --pull --force-rm --no-cache -f ./docker/nginx/Dockerfile -t $1_nginx:latest ./docker/nginx
-docker build --pull --force-rm --no-cache -f ./Dockerfile -t $1_php:latest .
+docker build --pull --force-rm --no-cache -f ./docker/nginx/Dockerfile -t $1/$2_nginx:latest ./docker/nginx
+docker build --pull --force-rm --no-cache -f ./Dockerfile -t $1/$2_php:latest .
