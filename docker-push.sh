146ae30833d12837298c6eefa50600310be898ff
Marc Ferland <mferland1980@gmail.com>
2020-01-02 19:26:05 -0500

    Updated syntax for docker-push.sh

--- docker-push.sh
+++ docker-push.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+if [ "$TRAVIS_BRANCH" = "master" ]; then
     docker push $1/$2_nginx:latest
     docker push $1/$2_php:latest
 fi
